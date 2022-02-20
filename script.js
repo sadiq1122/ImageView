f1f8b826b3c2709212bb3f89f91b024f3717c338
sadiq1122 <sadiq.977fg@gmail.com>
2022-02-19 22:59:32 -0800

    Fixed the initial configuration

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
 
 
-let views = document.getElementsByClassName('image-container');
-
-for (let i = 0; i < views.length; i++) {
-    views[i].addEventListener('click', () => { views[i].setAttribute('style', views[i].clientWidth === 40 ? 'width:700px' : 'width:40px'); setOthers(views,i) })
-    // if(i===0) {
-    // views[i].setAttribute('style','width:700px')
-    // setOthers(views,i)
-    }
-
-
-
-const setOthers = (elements,i) => {
-    
-    for(let j=0;j<elements.length ; j++)
-    {
-        if(j!==i)
-        {
-            elements[j].setAttribute('style','width:40px')
+const setOthers = (elements, i) => {
+    for (let j = 0; j < elements.length; j++) {
+        if (j !== i) {
+            elements[j].setAttribute('style', 'width:40px')
         }
     }
 
 } 
+let views = document.getElementsByClassName('image-container');
+setOthers(views, 1)
+for (let i = 0; i < views.length; i++) {
+    views[i].addEventListener('click', () => { views[i].setAttribute('style', views[i].clientWidth === 40 ? 'width:700px' : 'width:40px'); setOthers(views, i) })
+}
+
