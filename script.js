c9575934edd65d7214acb9d7737d45d7681ece8c
sadiq1122 <sadiq.977fg@gmail.com>
2022-02-19 23:11:50 -0800

    Fixed Indentation

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 
-
 const setOthers = (elements, i) => {
     for (let j = 0; j < elements.length; j++) {
         if (j !== i) {
             elements[j].setAttribute('style', 'width:40px')
         }
     }
+}
 
-} 
 let views = document.getElementsByClassName('image-container');
-setOthers(views, 1)
+
+setOthers(views, 0)
+
 for (let i = 0; i < views.length; i++) {
-    views[i].addEventListener('click', () => { views[i].setAttribute('style', views[i].clientWidth === 40 ? 'width:700px' : 'width:40px'); setOthers(views, i) })
+    views[i].addEventListener('click',
+        () => {
+            views[i].setAttribute('style', views[i].clientWidth === 40 ? 'width:700px' : 'width:40px');
+            setOthers(views, i)
+        })
 }
 
